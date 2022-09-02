d67799838d987d101918044bc83ea6a9c7d633ed
berg <berg.iodaniel@gmail.com>
2022-09-02 17:37:46 +0200

    finished admin page

---
+++ src/components/ProductTable/ProductTable.tsx
@@ -0,0 +1,194 @@
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import {
+  Box,
+  Button,
+  FormControl,
+  Input,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { FC, useState } from "react";
+import { Product } from "../../utils/types";
+import SortableTableColumnHead from "./SortableTableColumnHead";
+
+interface Props {
+  Products: Product[];
+  handleDelete: (product: Product) => void;
+  handleEdit: (product: Product) => void;
+  handleAdd: () => void;
+}
+
+type SortByType = "id" | "name" | "price" | "amountInStock";
+type SearchByType = "id" | "name" | "price" | "amountInStock" | "description";
+
+const ProductTable: FC<Props> = ({
+  Products,
+  handleDelete,
+  handleEdit,
+  handleAdd,
+}) => {
+  const [sortBy, setSortBy] = useState<SortByType>("id");
+  const [searchInput, setSearchInput] = useState("");
+  const [searchBy, setSearchBy] = useState<SearchByType>("name");
+  const [sortDirection, setSortDirection] = useState<"asc" | "desc">("desc");
+
+  const ProcessProducts = (products: Product[]) => {
+    const filtered = products.filter((product) =>
+      product[searchBy]
+        .toString()
+        .toLowerCase()
+        .includes(searchInput.toLowerCase())
+    );
+
+    if (sortDirection === "asc") {
+      return filtered.sort((a, b) => (a[sortBy] > b[sortBy] ? 1 : -1));
+    } else {
+      return filtered.sort((a, b) => (a[sortBy] < b[sortBy] ? 1 : -1));
+    }
+  };
+
+  const handleSort = (sortProperty: SortByType) => {
+    if (sortBy === sortProperty) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortBy(sortProperty);
+    }
+    console.log("sortBy", sortBy);
+  };
+
+  return (
+    <Box>
+      <TableContainer component={Paper}>
+        <Toolbar
+          sx={{ justifyContent: "space-between", backgroundColor: "#efefef" }}
+        >
+          <Typography variant="h6">Produkter</Typography>
+          <div className="flex justify-center align-center">
+            <FormControl size="small">
+              <InputLabel id="searchType-label">Sök Efter</InputLabel>
+              <Select
+                value={searchBy}
+                onChange={(e) => setSearchBy(e.target.value as SearchByType)}
+                size="small"
+                labelId="searchType-label"
+                label="Search By"
+                color="primary"
+                sx={{ mr: 2 }}
+              >
+                <MenuItem value={"id"}>Id</MenuItem>
+                <MenuItem value={"name"}>Namn</MenuItem>
+                <MenuItem value={"description"}>Beskrivning</MenuItem>
+                <MenuItem value="price">Pris</MenuItem>
+              </Select>
+            </FormControl>
+            <Input
+              onChange={(e) => setSearchInput(e.target.value)}
+              value={searchInput}
+              placeholder="Sök"
+            />
+            <Button
+              color="success"
+              variant="outlined"
+              sx={{ marginLeft: 2 }}
+              onClick={handleAdd}
+            >
+              Ny Produkt
+            </Button>
+          </div>
+        </Toolbar>
+        <Table sx={{ minWidth: 650 }} size="small">
+          <TableHead>
+            <TableRow>
+              <SortableTableColumnHead
+                align="left"
+                text="ID"
+                active={sortBy === "id"}
+                onClick={() => handleSort("id")}
+                sortDirection={sortDirection}
+              />
+              <SortableTableColumnHead
+                align="left"
+                text="Namn"
+                active={sortBy === "name"}
+                onClick={() => handleSort("name")}
+                sortDirection={sortDirection}
+              />
+              <TableCell>
+                <Typography variant="body2">BESKRIVNING</Typography>
+              </TableCell>
+              <SortableTableColumnHead
+                align="left"
+                text="Pris"
+                active={sortBy === "price"}
+                onClick={() => handleSort("price")}
+                sortDirection={sortDirection}
+              />
+              <SortableTableColumnHead
+                align="left"
+                text="Lager"
+                active={sortBy === "amountInStock"}
+                onClick={() => handleSort("amountInStock")}
+                sortDirection={sortDirection}
+              />
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {ProcessProducts(Products).map((product, idx) => (
+              <TableRow
+                key={product.id}
+                sx={idx % 2 == 0 ? { backgroundColor: "#f9f9f9" } : {}}
+              >
+                <TableCell>{product.id}</TableCell>
+                <TableCell>{product.name}</TableCell>
+                <TableCell
+                  sx={{
+                    maxWidth: 500,
+                    minWidth: 0,
+                    overflow: "hidden",
+                    textOverflow: "ellipsis",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  {product.description}
+                </TableCell>
+                <TableCell>{product.price}</TableCell>
+                <TableCell>{product.amountInStock}</TableCell>
+                <TableCell>
+                  <div className="flex">
+                    <Button
+                      color="info"
+                      size="small"
+                      onClick={() => handleEdit(product)}
+                    >
+                      <EditOutlinedIcon />
+                    </Button>
+                    <Button
+                      color="error"
+                      size="small"
+                      onClick={() => handleDelete(product)}
+                    >
+                      <DeleteForeverOutlinedIcon />
+                    </Button>
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
+
+export default ProductTable;
--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,10 +1,66 @@
-import { FC } from "react";
+import {
+  Button,
+  Container,
+  Dialog,
+  DialogActions,
+  DialogTitle,
+} from "@mui/material";
+import { FC, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProductTable from "../components/ProductTable/ProductTable";
+import { useProducts } from "../contexts/ProductContext";
+import { Product } from "../utils/types";
 
 const AdminPage: FC = () => {
+  const { products, removeProduct } = useProducts();
+  const [productToDelete, setProductToDelete] = useState<Product | null>(null);
+  const navigate = useNavigate();
+
+  const handleClickDelete = (product: Product) => {
+    setProductToDelete(product);
+  };
+
+  const handleConfirmDelete = (product: Product) => {
+    removeProduct(product);
+    setProductToDelete(null);
+  };
+
+  const handleClickEdit = (product: Product) => {
+    navigate(`/admin/edit/${product.id}`);
+  };
+
+  const handleClickAdd = () => {
+    navigate(`/admin/create`);
+  };
+
   return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
+    <>
+      <Container>
+        <div className="mt-6" />
+        <ProductTable
+          Products={products}
+          handleDelete={handleClickDelete}
+          handleEdit={handleClickEdit}
+          handleAdd={handleClickAdd}
+        />
+        <Dialog open={productToDelete !== null}>
+          <DialogTitle>
+            Är du säker på att du vill ta bort produkten "
+            {productToDelete?.name}"?
+          </DialogTitle>
+          <DialogActions>
+            <Button onClick={() => setProductToDelete(null)}>Avbryt</Button>
+            <Button
+              variant="contained"
+              onClick={() => handleConfirmDelete(productToDelete!)}
+              color="error"
+            >
+              Ta bort
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Container>
+    </>
   );
 };
 

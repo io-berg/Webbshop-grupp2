e859f9d9a58ace73a1836d9eeb85e9e247791e95
Eva-Li8E <evaliboden@gmail.com>
2022-09-05 13:09:56 +0200

    Hompage and HomeCard

---
+++ src/components/HomeCard.tsx
@@ -0,0 +1,46 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React, { FC } from "react";
+import { Product } from "../utils/types";
+
+interface Props {
+  product: Product;
+}
+const HomeCard: FC<Props> = ({ product }) => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={product.imgUrl}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {product.name}
+            <br />
+            {product.price} kr
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {product.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Lägg till i kundvagn
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default HomeCard;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,22 @@
 import { FC } from "react";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import HomeCard from "../components/HomeCard";
+import { grey } from "@mui/material/colors";
+import { data } from "../utils/mockData";
 
 const HomePage = () => {
   return (
-    <div>
-      <p>Test</p>
-    </div>
+    <Container>
+      <Grid container spacing={3} marginTop={1} marginBottom={4}>
+        {data.map((datan) => (
+          <Grid item xs={12} sm={6} md={3}>
+            <HomeCard product={datan} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 

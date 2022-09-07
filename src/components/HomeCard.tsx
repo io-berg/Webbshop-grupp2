0d435392eb7b962acb69ad83b2aad6e133c311f9
Eva-Li8E <evaliboden@gmail.com>
2022-09-07 10:43:10 +0200

    fix lint error

--- src/components/HomeCard.tsx
+++ src/components/HomeCard.tsx
@@ -1,6 +1,5 @@
 import {
   Button,
-  ButtonBaseActions,
   Card,
   CardActionArea,
   CardActions,
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,15 @@
-import { FC } from "react";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
 import Container from "@mui/material/Container";
 import HomeCard from "../components/HomeCard";
-import { grey } from "@mui/material/colors";
 import { data } from "../utils/mockData";
 
 const HomePage = () => {
   return (
     <Container>
       <Grid container spacing={3} marginTop={1} marginBottom={4}>
-        {data.map((datan) => (
-          <Grid item xs={12} sm={6} md={3}>
-            <HomeCard product={datan} />
+        {data.map((product) => (
+          <Grid key={product.id} item xs={12} sm={6} md={3}>
+            <HomeCard product={product} />
           </Grid>
         ))}
       </Grid>

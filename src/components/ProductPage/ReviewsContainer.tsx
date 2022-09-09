a9da9823c804f8955ddd72f0bd26dd4a94a94610
berg <berg.iodaniel@gmail.com>
2022-09-09 10:37:08 +0200

    moved rating stars

--- src/components/ProductPage/ReviewsContainer.tsx
+++ src/components/ProductPage/ReviewsContainer.tsx
@@ -12,11 +12,11 @@ const ReviewsContainer: FC<Props> = ({ reviews }) => {
       <Typography variant="h5">Recensioner</Typography>
       {reviews.map((review, idx) => (
         <Box key={idx}>
-          <Typography variant="h6">{review.title}</Typography>
+          <Typography variant="h6">{review.title} </Typography>
+          <Rating name="read-only" value={review.stars} readOnly />
           <Typography variant="body2" color="text.secondary">
-            <Typography component="legend">{review.body}</Typography>
+            <Typography component="legend">{review.body}</Typography>-{" "}
             {review.author}
-            <Rating name="read-only" value={review.stars} readOnly />
           </Typography>
         </Box>
       ))}

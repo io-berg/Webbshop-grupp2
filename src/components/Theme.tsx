c1106aa00e5dcfb954d904cd4d37714075835d0d
Isabella Ekdahl <isabellaekdahl@outlook.com>
2022-09-09 11:30:59 +0200

    add green theme

---
+++ src/components/Theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material/styles";
+import { green } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: green[500],
+    },
+  },
+});
+
+export default theme;
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,15 +6,18 @@ import App from "./App";
 import ProductProvider from "./contexts/ProductContext";
 import SnackProvider from "./contexts/SnackContext";
 import "./styles/global.css";
+import theme from "./components/Theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductProvider>
-        <SnackProvider>
-          <App />
-        </SnackProvider>
-      </ProductProvider>
+      <ThemeProvider theme={theme}>
+        <ProductProvider>
+          <SnackProvider>
+            <App />
+          </SnackProvider>
+        </ProductProvider>
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

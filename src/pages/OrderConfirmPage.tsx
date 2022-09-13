860c85a1b5f10ba10453bd467eb7f31a258b7307
Linus Nilsson <linus.erik.nilsson@outlook.com>
2022-09-13 13:26:59 +0200

    Fixed formatting

--- src/pages/OrderConfirmPage.tsx
+++ src/pages/OrderConfirmPage.tsx
@@ -1,32 +1,26 @@
-import { Box, Button, Container } from "@mui/material"
-import { FC } from "react"
-import NavCrumbs from "../components/NavCrumbs"
-
+import { Box, Button, Container } from "@mui/material";
+import { FC } from "react";
+import NavCrumbs from "../components/NavCrumbs";
 
 const OrderConfirmPage: FC = () => {
-
   return (
     <Container>
-    <NavCrumbs
+      <NavCrumbs
         crumbs={[
           { name: "Home", path: "/" },
           { name: "OrderConfirmation", path: "/orderconfirmation" },
-        ]} />
+        ]}
+      />
 
-        <Box>
-            <h1>
-                Tack för din beställning! 🌻
-            </h1>
+      <Box>
+        <h1>Tack för din beställning! 🌻</h1>
 
-          <Button href="/" variant="contained">
-            Tillbaka till startsidan
-          </Button>
-
-        </Box>
-        
+        <Button href="/" variant="contained">
+          Tillbaka till startsidan
+        </Button>
+      </Box>
     </Container>
-    
-
-)};
+  );
+};
 
 export default OrderConfirmPage;
